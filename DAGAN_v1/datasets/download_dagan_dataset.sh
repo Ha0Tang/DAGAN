a29fedeb1a9c8e9329d3fab2f40a0af404591545
Ha0Tang <haotang@pku.edu.cn>
2020-09-12 22:48:21 +0200

    upload dataset download scripts

---
+++ DAGAN_v1/datasets/download_dagan_dataset.sh
@@ -0,0 +1,17 @@
+FILE=$1
+
+if [[ $FILE != "facades" && $FILE != "deepfashion" && $FILE != "celeba"]]; 
+	then echo "Available datasets are facades, deepfashion, celeba"
+	exit 1
+fi
+
+
+echo "Specified [$FILE]"
+
+URL=http://disi.unitn.it/~hao.tang/uploads/datasets/DAGAN/$FILE.tar.gz
+TAR_FILE=./datasets/$FILE.tar.gz
+TARGET_DIR=./datasets/$FILE/
+wget -N $URL -O $TAR_FILE
+mkdir -p $TARGET_DIR
+tar -zxvf $TAR_FILE -C ./datasets/
+rm $TAR_FILE
