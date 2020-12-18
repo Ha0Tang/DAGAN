d7c713fb8bad193aa34d9b3c1fbd878194607878
Ha0Tang <haotang@pku.edu.cn>
2020-12-18 16:49:09 +0100

    fix typos

--- DAGAN_v1/test_ade.sh
+++ DAGAN_v1/test_ade.sh
@@ -1,1 +1,1 @@
-python test.py --name GauGAN_DAGAN_ade --dataset_mode ade20k --dataroot ./datasets/ADEChallengeData2016 --gpu_ids 0 --results_dir ./results --checkpoints_dir ./checkpoints --batchSize 1 --which_epoch 200;
+python test.py --name GauGAN_DAGAN_ade --dataset_mode ade20k --dataroot ./datasets/ade20k --gpu_ids 0 --results_dir ./results --checkpoints_dir ./checkpoints --batchSize 1 --which_epoch 200;
--- DAGAN_v1/train_ade.sh
+++ DAGAN_v1/train_ade.sh
@@ -1,3 +1,3 @@
 #export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7;
-python train.py --name GauGAN_DAGAN_ade --dataset_mode ade20k --dataroot ./datasets/ADEChallengeData2016 --niter 100 --niter_decay 100 --gpu_ids 0 --checkpoints_dir ./checkpoints --batchSize 8 --save_epoch_freq 5 --save_latest_freq 1000 
+python train.py --name GauGAN_DAGAN_ade --dataset_mode ade20k --dataroot ./datasets/ade20k --niter 100 --niter_decay 100 --gpu_ids 0 --checkpoints_dir ./checkpoints --batchSize 8 --save_epoch_freq 5 --save_latest_freq 1000 
 # --continue_train;
